236dfb8ee107a2fae689011db4560f55078b7082
rust404 <rust404@outlook.com>
2020-08-29 17:50:47 +0800

    初步完成button组件的基本样式

--- docs/components/basic/button.md
+++ docs/components/basic/button.md
@@ -1,5 +1,23 @@
 # Button
 
+## 类型
 <template>
-    <w-button/>
+    <w-button>Default</w-button>
+    <w-button type="primary">Primary</w-button>
+    <w-button type="danger">Danger</w-button>
+    <w-button type="success">Success</w-button>
+    <w-button type="info">Info</w-button>
+</template>
+
+## 大小
+<template>
+    <w-button type="primary">normal</w-button>
+    <w-button size="lg">Large</w-button>
+    <w-button size="sm" type="danger">Danger</w-button>
+</template>
+
+## 禁用
+<template>
+    <w-button type="primary">normal</w-button>
+    <w-button type="primary" disabled>disabled</w-button>
 </template>
--- src/packages/button/button.vue
+++ src/packages/button/button.vue
@@ -1,9 +1,40 @@
 <template>
-  <button>hello</button>
+  <button :class="className">
+    <span><slot/></span>
+  </button>
 </template>
 
 <script>
 export default {
-  name: 'w-button'
+  name: 'w-button',
+  props: {
+    type: {
+      type: String,
+      default: 'default',
+      validator(prop) {
+        return ['primary', 'danger', 'warning', 'info', 'success', 'default'].indexOf(prop) !== -1
+      }
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    size: {
+      type: String,
+      validator(prop) {
+        return ['lg', 'sm', ''].indexOf(prop) !== -1
+      }
+    }
+  },
+  computed: {
+    className() {
+      return {
+        'wing-btn': true,
+        [`wing-btn-${this.type}`]: this.type,
+        [`wing-btn-${this.size}`]: this.size,
+        'is-disabled': this.disabled,
+      }
+    }
+  }
 }
 </script>
