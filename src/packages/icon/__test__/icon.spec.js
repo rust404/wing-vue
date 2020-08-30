1a3d4c1db9a64f8aecd3936b74ba47cf164ad423
rust404 <rust404@outlook.com>
2020-08-30 18:22:13 +0800

    补充icon组件

---
+++ src/packages/icon/__test__/icon.spec.js
@@ -0,0 +1,6 @@
+import { shallowMount } from '@vue/test-utils'
+import Icon from '../icon.vue'
+
+describe('Icon component', () => {
+})
+  
---
+++ src/packages/icon/icon.vue
@@ -0,0 +1,43 @@
+<template>
+  <span class="w-icon" :style="wrapperStyle" :spin="this.spin" v-html="this.icons[name]">
+  </span>
+</template>
+
+<script>
+import iconPaths from './iconPaths';
+
+export default {
+  name: 'w-icon',
+  props: {
+    name: String,
+    size: {
+      type: String,
+      default: '1em'
+    },
+    spin: Boolean,
+    color: {
+      type: String
+    }
+  },
+  created() {
+    this.icons = iconPaths
+  },
+  computed: {
+    wrapperStyle() {
+      return {
+        width: this.size,
+        height: this.size,
+        color: this.color,
+      }
+    }
+  },
+  data() {
+    return {
+      icons: {}
+    }
+  },
+  render(h) {
+
+  }
+}
+</script>
